aa3e100f89565fb08e1c98014bead23a21572794
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-20 03:27:10 -0400

    4 different choices handled by if and else if

--- bank.go
+++ bank.go
@@ -1,6 +1,8 @@
 package main
 import "fmt"
 func main(){
+	var accountBalance float64= 1000
+
 	fmt.Println("Welcome to Go Bank!!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check Balance")
@@ -10,6 +12,26 @@ func main(){
 
 	var choice int
 	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Your Balance is: ", accountBalance)
+	}else if choice == 2 {
+		fmt.Println("Your Deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+
+		accountBalance += depositAmount
+		fmt.Println("Balance Updated! New Amount: ", accountBalance)
+	}else if choice == 3 {
+		fmt.Println("Your Amount: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+
+		accountBalance -= withdrawAmount
+		fmt.Println("Balance Updated! New Amount: ",accountBalance)
+	}else {
+		fmt.Println("GoodBye!!!")
+	}
 
 	fmt.Println("Your Choice:", choice)
 
