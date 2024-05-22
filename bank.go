d67c25f95703f393f5e961ad6e17bdafb31feddb
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-22 01:08:56 -0400

    added goals

--- bank.go
+++ bank.go
@@ -1,99 +1,37 @@
 package main
+
 import "fmt"
-import "os"
-import "strconv"
-import "errors"
 
+// Goals
 
-const accountBalanceFile = "balance.txt"
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)   // generate a string 
-	// []byte(balanceText)  converting string into bytes
-	os.WriteFile(accountBalanceFile,[]byte(balanceText),0644) //0644 is a file permission to read and write file from owner
+// 1) Validate User Input
+// 	=> Show error message and exit if Invalid input is provided
+// 	- No Negative Numbers
+// 	- Not 0
+// 2) Store Calculated results into file
+
+func main() {
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
+
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
 }
 
-func getBalanceFromFile() (float64,error){
-	// _ means we don't wanna work with it right now
-	data, err := os.ReadFile(accountBalanceFile)
-	// nil is a special value in go which stands for the absence
-	if err != nil {	// Handling error
-		return 1000, errors.New("failed To Find Balance File")
-	}
-	balanceText := string(data)
-	balance , err := strconv.ParseFloat(balanceText, 64) // convert string to float
-	if err != nil {			// if it can't convert string to float
-		return 1000, errors.New("failed To Parse stored balance value")
-	}
-	return balance, nil
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
 }
 
-func main(){
-	var accountBalance, err= getBalanceFromFile()
-	if err != nil {
-		fmt.Println("ERROR")
-		fmt.Println(err)
-		fmt.Println("-------------")
-		panic("Can't continue Sorry!") // built in Panic function help in stop the execution of program
-		// return // if don't want to continue execution if there is error
-
-	}
-	fmt.Println("Welcome to Go Bank!!")
-
-   for { 	
-
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
-
-	var choice int
-	fmt.Scan(&choice)
-
-	switch choice {
-		case 1:
-			fmt.Println("Your Balance is: ", accountBalance)
-		case 2:
-			fmt.Println("Your Deposit: ")
-			var depositAmount float64
-			fmt.Scan(&depositAmount)
-
-			if depositAmount < 0 {
-				fmt.Println("Invalid DepositAmount! Must be greater than 0!")
-				// return // stops the execution of function
-				continue //skip the current loop and iterate next
-			}
-
-			accountBalance += depositAmount
-			fmt.Println("Balance Updated! New Amount: ", accountBalance)
-			writeBalanceToFile(accountBalance)
-		case 3: 
-			fmt.Println("Your Amount: ")
-			var withdrawAmount float64
-			fmt.Scan(&withdrawAmount)
-
-			if withdrawAmount < 0 {
-				fmt.Println("Invalid DepositAmount! Must be greater than 0!")
-				// return // stops the execution of function
-				continue 
-			}
-
-			if withdrawAmount > accountBalance {
-				fmt.Println("Invalid Amount! You can't withdraw more tha what you have!")
-				// return
-				continue 
-			}
-
-			accountBalance -= withdrawAmount
-			fmt.Println("Balance Updated! New Amount: ",accountBalance)
-		default:
-			fmt.Println("GoodBye!!!")
-			return
-			//break
-
-	}
-
-}
-	fmt.Println("Thanks for choosing our bank!")
-   
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
 }
