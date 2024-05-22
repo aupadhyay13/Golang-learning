0243444f0bba0c12fa1361c9bc7172a70cd62ecf
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-22 01:37:36 -0400

    completed exercise

--- bank.go
+++ bank.go
@@ -1,6 +1,8 @@
 package main
 
 import "fmt"
+import "errors"
+import "os"
 
 // Goals
 
@@ -10,13 +12,30 @@ import "fmt"
 // 	- Not 0
 // 2) Store Calculated results into file
 
+func validateUserInput(revenue,expenses,taxRate float64) error {
+	if revenue <= 0 || expenses <= 0 || taxRate <= 0 {
+		return errors.New("invalid Input Provided! No Negative Number")
+	}
+	return nil
+}	
+
+func writeResultIntoFile(ebt,profit,ratio float64){
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n",ebt,profit,ratio) //generate string with formatting 
+	os.WriteFile("result.txt",[]byte(results),0644)
+}
 func main() {
 	revenue := getUserInput("Revenue: ")
 	expenses := getUserInput("Expenses: ")
 	taxRate := getUserInput("Tax Rate: ")
-
+	err := validateUserInput(revenue,expenses,taxRate)
+	if err != nil{
+		fmt.Println("Error")
+		fmt.Println(err)
+		return
+	}
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
+	writeResultIntoFile(ebt, profit, ratio)
 	fmt.Printf("%.1f\n", ebt)
 	fmt.Printf("%.1f\n", profit)
 	fmt.Printf("%.3f\n", ratio)
