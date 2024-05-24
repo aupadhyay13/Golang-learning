d553c66c8e4ac98f86f1b364cf6391c0b9e11322
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-23 20:58:20 -0400

    instantiating a struct and struct literal notation

--- structs.go
+++ structs.go
@@ -10,15 +10,24 @@ type user struct {		 // if first character is uppercase then it is globally avai
 	firstName string
 	lastName string
 	birthDate string
-	createdAt time.Time
+	createdAt time.Time  // nested struct
 } 
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
 	// ... do something awesome with that gathered data!
 
+	var appUser user
+
+
+	appUser = user {
+		firstName: userFirstName,
+		lastName: userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
 	outputUserDetails(firstName, lastName, birthdate)
 }
 
