e0023bf94dc5b458a1b77c78e7bbb3bb1a47e979
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-24 02:13:09 -0400

    use of structs tags

--- note/note.go
+++ note/note.go
@@ -7,9 +7,9 @@ import "strings"
 import "encoding/json"
 
 type Note struct {
-	Title string
-	Content string
-	CreatedAt time.Time
+	Title string `json:"title"`
+	Content string `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 func (note Note) DisplayNote() {
 	fmt.Printf("Your Note Titled %v has the following content: \n\n%v\n\n ",note.Title, note.Content)
