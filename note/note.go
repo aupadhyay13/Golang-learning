fade4c011f0a9ccfef3c364babfcd83bdff73ad3
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-24 01:58:56 -0400

    created Save method

--- note/note.go
+++ note/note.go
@@ -2,6 +2,8 @@ package note
 import "time"
 import "fmt"
 import "errors"
+import "os"
+import "strings"
 
 type Note struct {
 	title string
@@ -11,6 +13,12 @@ type Note struct {
 func (note Note) DisplayNote() {
 	fmt.Printf("Your Note Titled %v has the following content: \n\n%v\n\n ",note.title, note.content)
 } 
+
+func (note Note) Save(){
+	fileName := strings.ReplaceAll(note.title, " ", "_")
+	fileName = strings.ToLower(fileName)
+	os.WriteFile(fileName,)
+}
 
 func New(title, content string) (Note,error){
 	if title == "" || content == ""{
