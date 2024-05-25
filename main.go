e02ec4e51af0d93b597ae0e3dabb6ffa100f1928
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-24 21:11:11 -0400

    createing todu struct and save and display todo

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 import "fmt"
 import "example.com/struct-project/note"
+import "example.com/struct-project/todo"
 import "bufio"
 import "os"
 import "strings"
 
 func main(){
 	title, content := getNoteData()
+	todoText := getUserInput("To Do Text:")
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	userNote,err := note.New(title,content)
 	if err != nil {
@@ -14,6 +21,14 @@ func main(){
 		return
 	}
 
+	todo.DisplayToDo()
+	err = todo.Save()
+	if err!= nil {
+		fmt.Println("Saving the Todo failed!")
+		return 
+	}
+
+	fmt.Println("Saving the Todo succeeded!")
 	userNote.DisplayNote()
 	err = userNote.Save()
 	if err!= nil {
@@ -31,6 +46,8 @@ func getNoteData() (string,string){
 	content := getUserInput("Note Content:")
 	return title,content
 }
+
+
 
 func getUserInput(prompt string) (string){
 	fmt.Printf("%v ",prompt)
