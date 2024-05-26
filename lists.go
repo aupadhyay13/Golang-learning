af331c1a10e02b7ce50da13af90cf66e61642540
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-26 00:17:19 -0400

    building dynamic lists with slices

--- lists.go
+++ lists.go
@@ -2,21 +2,10 @@ package main
 import "fmt"
 
 func main(){
-	var productNames [4]string = [4]string{"A book"}
-	prices := [4]float64{10.99,9.99,45.99,20.00}
+	prices := []float64{10.99,8.99}	
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
+	updatedPrices := append(prices, 5.99)
+	fmt.Println(updatedPrices)
 	fmt.Println(prices)
-
-	productNames[2] = "A Carpet"
-	fmt.Println(productNames)
-
-	fmt.Println(prices[2])
-	// featuredPrices := prices[:3]  // from begineeing
-	featuredPrices := prices[1:]
-	featuredPrices[0] = 199.99 // though we changed in featuredPrice it will change in price array as well
-	highlitedPrices := featuredPrices[:1]
-	fmt.Println(highlitedPrices)
-
-	fmt.Println(featuredPrices)
-	fmt.Println(prices)
-	fmt.Println(len(highlitedPrices), cap(highlitedPrices))
 }
