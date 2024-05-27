f8eb709429837a9a6cf24131e3fe8ba1a310d583
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-05-27 00:27:43 -0400

    Mutating Maps

--- maps.go
+++ maps.go
@@ -7,4 +7,10 @@ func main() {
 		"Amazon Web Services" : "https://aws.com",
 	}
 	fmt.Println(websites)
+	fmt.Println(websites["Amazon Web Services"])
+	websites["LinkedIn"] = "https://lkinkedin.com"
+	fmt.Println(websites)
+
+	delete(websites, "Google")
+	fmt.Println(websites)
 }
