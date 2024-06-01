851d560f93b26670992bfa17fea61f04dba54ea4
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-06-01 16:05:58 -0400

    recursion

--- main.go
+++ main.go
@@ -1,51 +1,22 @@
-//functions are nothing but value in golang
-
 package main
 import "fmt"
-
-type funcType func(int) int
-
 func main(){
-	numbers := []int{1,2,3,4}
-	moreNUmbers := []int{5,1,2}
-
-	triple := tranformNumbers(&numbers, triple)
-	doubled := tranformNumbers(&numbers, double)
-	fmt.Println(doubled)
-	fmt.Println(triple)
-
-	tranformerFn1 := getTranformerFunction(&numbers)
-	tranformerFn2 := getTranformerFunction(&moreNUmbers)
-
-	tranformedNums := tranformNumbers(&numbers, tranformerFn1)
-	moreTranformedNums := tranformNumbers(&moreNUmbers, tranformerFn2)
-
-	fmt.Println(tranformedNums)
-	fmt.Println(moreTranformedNums)
+	fact := factorial(6)
+	fmt.Println(fact)
 }
 
-func getTranformerFunction(number *[]int) funcType{
-	if((*number)[0] == 1){
-		return double
-	}else{
-		return triple
-	}
+func factorial (number int) int {
 	
-}
-
-func tranformNumbers(numbers *[]int, tranform funcType) []int{
-	dNumbers := []int{}
-	for _,value := range *numbers{
-		dNumbers = append(dNumbers,tranform(value))
+	if(number == 0){
+		return 1
 	}
+	return number * factorial(number-1)
+	
+	// result := 1
+	// for i := 1;i <= number; i++{
+	// 	result = result * i;
+	// }
+	// return result
 
-	return dNumbers
-}
 
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
