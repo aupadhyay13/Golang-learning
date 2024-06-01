1cd6997d1ab396961ab61433654f9000b88302cf
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-06-01 17:34:22 -0400

    basic tax calculator

--- main.go
+++ main.go
@@ -1,22 +1,18 @@
 package main
 import "fmt"
 func main(){
-	fact := factorial(6)
-	fmt.Println(fact)
-}
+	var prices []float64 = []float64{10,20,30}
+	var taxRates []float64 = []float64{0,0.07,0.1,0.15}
 
-func factorial (number int) int {
-	
-	if(number == 0){
-		return 1
+	result  := make(map[float64][]float64) // create empty map
+
+	for _,taxRate := range taxRates {
+		var taxIncludedPrices []float64 = make([]float64,len(prices)) 
+		for priceIndex,price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1+taxRate)
+		}
+
+		result[taxRate] = taxIncludedPrices
 	}
-	return number * factorial(number-1)
-	
-	// result := 1
-	// for i := 1;i <= number; i++{
-	// 	result = result * i;
-	// }
-	// return result
-
-
+	fmt.Println(result)
 }
