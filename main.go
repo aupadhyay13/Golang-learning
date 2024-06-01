cdc61c86c12e5ea0ecc0cf1db22b0bcf779a0480
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-06-01 15:22:40 -0400

    passing function in arguments

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
-package main
+//functions are nothing but value in golang
 
+package main
 import "fmt"
 
+type funcType func(int) int
+
 func main(){
-	userNames := make([]string,2, 5) // 5 is maximum capacity
+	numbers := []int{1,2,3,4}
+	triple := tranformNumbers(&numbers, triple)
+	doubled := tranformNumbers(&numbers, double)
+	fmt.Println(doubled)
+	fmt.Println(triple)
+}
 
-	userNames = append(userNames,"Aditya")
-	userNames = append(userNames,"Jimit")
+func tranformNumbers(numbers *[]int, tranform funcType) []int{
+	dNumbers := []int{}
+	for _,value := range *numbers{
+		dNumbers = append(dNumbers,tranform(value))
+	}
 
-	fmt.Println(userNames)
+	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }
