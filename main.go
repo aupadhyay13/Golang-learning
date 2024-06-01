bf9d45dd69fbf8f1bdb8f583a7f29e0892b745a5
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-06-01 15:32:46 -0400

    returning functions as value

--- main.go
+++ main.go
@@ -7,10 +7,30 @@ type funcType func(int) int
 
 func main(){
 	numbers := []int{1,2,3,4}
+	moreNUmbers := []int{5,1,2}
+
 	triple := tranformNumbers(&numbers, triple)
 	doubled := tranformNumbers(&numbers, double)
 	fmt.Println(doubled)
 	fmt.Println(triple)
+
+	tranformerFn1 := getTranformerFunction(&numbers)
+	tranformerFn2 := getTranformerFunction(&moreNUmbers)
+
+	tranformedNums := tranformNumbers(&numbers, tranformerFn1)
+	moreTranformedNums := tranformNumbers(&moreNUmbers, tranformerFn2)
+
+	fmt.Println(tranformedNums)
+	fmt.Println(moreTranformedNums)
+}
+
+func getTranformerFunction(number *[]int) funcType{
+	if((*number)[0] == 1){
+		return double
+	}else{
+		return triple
+	}
+	
 }
 
 func tranformNumbers(numbers *[]int, tranform funcType) []int{
