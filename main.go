414b81d411fae877a802b57acf082751825d3d72
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-06-12 17:18:54 -0400

    registered a POST route

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
+
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"example.com/rest-api/models"
 )
 func main(){
 	server := gin.Default()
 
 	server.GET("/events", getEvents)	//GET POST PUT PATCH DELETE
+	server.POST("/events")
 
 	server.Run(":8081")	//localhost during development
 }
 
 func getEvents(context *gin.Context){
-	context.JSON(http.StatusOK, gin.H{"message" : "Hello!"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+
+func createEvent(context *gin.Context){
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil{
+		context.JSON(http.StatusBadRequest, gin.H{"message" : "Could not Parse Data!"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message" : "Event Created!", "event" : event})
 }
--- models/event.go
+++ models/event.go
@@ -3,10 +3,10 @@ import "time"
 
 type Event struct{
 	ID int 
-	Name string
-	Description string
-	Location string
-	DateTime time.Time
+	Name string `binding:"required"`
+	Description string	`binding:"required"`
+	Location string	`binding:"required"`
+	DateTime time.Time	`binding:"required"`
 	UserID int
 }
 
@@ -16,4 +16,9 @@ func (e Event) Save(){
 	// later: add it to the database
 
 	events = append(events,e)
+}
+
+func GetAllEvents() []Event {
+
+	return events
 }
