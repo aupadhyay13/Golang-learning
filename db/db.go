7ba3d59b22ad5ebbf38782019e52460fedf223ba
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-06-18 22:32:31 -0400

    added update and delete event APIS and created users table

--- db/db.go
+++ db/db.go
@@ -1,8 +1,9 @@
 package db
 
-import(
-	"fmt"
+import (
 	"database/sql"
+	"fmt"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -11,7 +12,7 @@ var DB *sql.DB
 func InitDB() {
 	var err error
 	DB, err = sql.Open("sqlite3", "api.db")
-	if err != nil{
+	if err != nil {
 		panic("Could not connect to Databse!")
 	}
 
@@ -21,7 +22,21 @@ func InitDB() {
 	createTables()
 }
 
-func createTables(){
+func createTables() {
+
+	createUserTable := `
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			email NOT NULL UNIQUE,
+			password TEXT NOT NULL
+		)
+	`
+
+	_, err := DB.Exec(createUserTable)
+	if err != nil {
+		panic("Clould not create user table!")
+	}
+
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,14 +45,13 @@ func createTables(){
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
 		user_id INTEGER
+		FOREIGN KEY(user_id) REFERENCES users
 	)
 	`
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 
-	if err != nil{
-		fmt.Println("error is--->",err)
+	if err != nil {
+		fmt.Println("error is--->", err)
 		panic("Could not create Database!")
 	}
 }
-
-
--- routes/events.go
+++ routes/events.go
@@ -56,3 +56,58 @@ func createEvent(context *gin.Context) {
 	fmt.Println("event is--->", event)
 	context.JSON(http.StatusCreated, gin.H{"message": "Event Created!", "event": event})
 }
+
+func updateEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parsed event ID."})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event.Try agin later."})
+		return
+	}
+
+	var updatedEvent models.Event
+	err = context.ShouldBindJSON(&updatedEvent)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	updatedEvent.ID = eventId
+	err = updatedEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event."})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event Updated Successfully!!"})
+}
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parsed event ID."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event.Try agin later."})
+		return
+	}
+
+	err = event.Delete()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not Delete event.Try agin later."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event Deleted Successfully!!"})
+
+}
