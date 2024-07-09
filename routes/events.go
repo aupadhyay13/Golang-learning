6bd615ae84cffd9d034de696f45e1a4d60bc51cc
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-07-08 22:33:46 -0400

    added token verification

--- routes/events.go
+++ routes/events.go
@@ -37,6 +37,13 @@ func getEvents(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
+		return
+	}
+
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 
