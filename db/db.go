0d9558f3626755927f1ee2859fe8f8f0df83d980
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-07-12 22:48:34 -0400

    added registration table

--- db/db.go
+++ db/db.go
@@ -54,4 +54,19 @@ func createTables() {
 		fmt.Println("error is--->", err)
 		panic("Could not create Database!")
 	}
+
+	createRegistrationTable := `
+		CREATE TABLE IF NOT EXISTS registrations (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			event_id INTEGER,
+			user_id INTEGER,
+			FOREIGN KEY(event_id) REFERENCES events(id),
+			FOREIGN KEY(user_id) REFERENCES users(id),
+		)
+	`
+
+	_, err = DB.Exec(createRegistrationTable)
+	if err != nil {
+		panic("could not create registration table")
+	}
 }
