ff71ba7185cc62886b6f8d538d1a233ae8622b6e
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-07-12 21:38:19 -0400

    retriveing and storing user in event

--- utils/jwt.go
+++ utils/jwt.go
@@ -21,32 +21,32 @@ func GenerateToken(email string, userId int64) (string, error) {
 	return token.SignedString([]byte(secretKey))
 }
 
-func VerifyToken(token string) error {
+func VerifyToken(token string) (int64, error) {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 
 		if !ok {
 			return nil, errors.New("unexpected signing method")
 		}
-		return secretKey, nil
+		return []byte(secretKey), nil
 	})
 
 	if err != nil {
-		return errors.New("could not parsed token.")
+		return 0, errors.New("could not parsed token")
 	}
 
 	tokenIsValid := parsedToken.Valid
 	if !tokenIsValid {
-		return errors.New("invalid Token")
+		return 0, errors.New("invalid Token")
 	}
 
-	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 
-	// if !ok {
-	// 	return errors.New("Invalid token claims!")
-	// }
+	if !ok {
+		return 0, errors.New("invalid token claims")
+	}
 
 	// email := claims["email"].(string)
-	// userId := claims["userId"].(int64)
-	return nil
+	userId := int64(claims["userId"].(float64))
+	return userId, nil
 }
