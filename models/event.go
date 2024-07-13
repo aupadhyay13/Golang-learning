f3d32b7566ef861f2b34a34d2d7ef1aeef00f492
Aditya Upadhyay <adityaupadhyay.1311@gmail.com>
2024-07-12 23:08:45 -0400

    created register function

--- models/event.go
+++ models/event.go
@@ -106,3 +106,15 @@ func (event Event) Delete() error {
 	_, err = stmt.Exec(event.ID)
 	return err
 }
+
+func (e Event) Register(userId int64) error {
+	query := "INSERT INTO TABLE registrations(event_id,user_id) VALUES (?,?)"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	_, err = stmt.Exec(e.ID, userId)
+	return err
+}
